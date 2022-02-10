b06663e70609530a25f5cb42364a42e9b0ccffcb
ishwar <ishwargowda100@gmail.com>
2022-02-10 21:39:24 +0530

    added DISTK problem

---
+++ miscellaneous/DISTK.cpp
@@ -0,0 +1,99 @@
+/*
+	https://www.codechef.com/problems/DISTK
+*/
+
+# include <bits/stdc++.h>
+using namespace std;
+namespace my {
+    template < typename T > class vector : public std::vector<T> {    public:
+        vector()                            { }
+        vector(int n)                       {    this->reserve(n); }
+        vector(std::initializer_list<T> ls) {    for (auto i : ls)	this->push_back(i); }
+        void push_input(size_t n)           {    for (size_t i { 0ULL }; i != n; ++i) {    T store;	cin >> store;    this->push_back(store);    }}
+    };
+}
+
+int recur_depth = 0;
+#ifdef DEBUG
+#define dbg(x) {++recur_depth; auto x_=x; --recur_depth; cerr<<string(recur_depth, '\t')<<"\e[91m"<<__func__<<":"<<__LINE__<<"\t"<<#x<<" = "<<x_<<"\e[39m"<<endl;}
+#else
+#define dbg(x)
+#endif
+template<typename Ostream, typename Cont>
+typename enable_if<is_same<Ostream, ostream>::value, Ostream&>::type operator<<(Ostream& os, const Cont& v) {
+    os << "[";
+    for (auto& x : v) { os << x << ", "; }
+    return os << "]";
+}
+template<typename T1, typename T2>           std::istream& operator>> (std::istream& in, pair<T1, T2>& P)      {    in >> P.first >> P.second;  return in;  }
+template<typename Ostream, typename ...Ts>   Ostream&      operator<<(Ostream& os, const pair<Ts...>& p)       {    return os << "{" << p.first << ", " << p.second << "}";                }
+template < typename T >                      inline bool   compare_float(T a, T b)                             {    return (abs(a - b) < 1e-9) ? true : false;                                   }
+template < typename T >                      istream&      operator>>(istream& os, vector<T>& v)               {    T store;    os >> store;    v.push_back(store);    return os;                }
+template < typename T >                      void          print(T container, string sep = " ", string end = "\n") {    for (const auto& i : container) std::cout << i << sep; cout << end; }
+
+const std::string  sp  { " " };
+const std::string  nl  { "\n" };
+#define sec second
+#define fir first
+/*******************************************************</Utilities>********************************************************/
+
+
+
+
+class solution {
+    int T = 1;
+    int64_t N;
+    int64_t K;
+public:
+    solution() {
+        /*some_precomputation*/
+    }
+
+    int find_x() {
+        int x = 0;
+        for(int64_t i = 1; i <= N; ++i){
+            int64_t store { i * (i + 1) / 2 };
+            if (store > K) {
+                return x;
+            }
+            x = i;
+        }
+        return x;
+    }
+
+    void solve() {
+        cin >> N >> K;
+
+//# </DON'T PANIC RELAX>
+
+        K = K - N;
+        int64_t x = find_x();
+        K = K - x * (x + 1) / 2;
+        dbg(K);
+        for (int i = 0; i < N; ++i) {
+            if (i <= x) {
+                cout << i + 1 << sp;
+            } else {
+                cout << x - K + 1 << sp;
+            }
+        }
+        cout << nl;
+    }
+
+    void operator()() {
+        #warning MULTIPLE TEST CASES WILL BE EXECUTED
+        std::cin >> T;
+        while (T--) {
+            solve();
+        }
+    }
+};
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solution()();
+    return 0;
+}
